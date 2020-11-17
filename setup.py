d86aec471bce2106fb3aa40d24898d7fc08d7ad8
Sebastian Otálora <sebastianffx@gmail.com>
2020-11-17 18:16:34 +0100

    Fixing imports

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stainlib',
-    version='0.5',
+    version='0.6.1',
     description='A library for H&E image stain normalization and augmentation.',
     author='Sebastian Otálora',
     author_email='sebastianffx@gmail.com',
